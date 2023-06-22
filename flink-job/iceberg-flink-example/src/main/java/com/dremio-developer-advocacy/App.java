afe891e71a7184477bbbe8902b6214d4f0a0084c
Alex Merced <alex@alexmerced.dev>
2023-06-22 10:27:31 -0400

    initial push

---
+++ flink-job/iceberg-flink-example/src/main/java/com/dremio-developer-advocacy/App.java
@@ -0,0 +1,13 @@
+package com.dremio-developer-advocacy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
